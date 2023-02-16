d0631db22e4079c57ba281708cee61763ebf1605
Luca <luca-eidos@gmail.com>
2023-02-16 15:39:15 +0100

    fix dichiarazione p

--- Esempi/03-Primes/c/lu_determinant.c
+++ Esempi/03-Primes/c/lu_determinant.c
@@ -93,7 +93,7 @@ int main(int argc, char **argv)
   }
 
   double **a = (double **)(malloc(sizeof(double *) * N));
-  int p[N + 1];
+  int *p = (int *)malloc(sizeof(int) * N);
   srand(time(NULL));
 
   for (int i = 0; i < N; i++)
