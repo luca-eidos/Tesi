b82341339c86daeaf6f3ab742b480fd802b77bcd
Luca <luca-eidos@gmail.com>
2023-02-17 14:25:25 +0100

    benchmark test update

--- Esempi/03-Primes/compile_and_benchmark.sh
+++ Esempi/03-Primes/compile_and_benchmark.sh
@@ -5,7 +5,7 @@ OUTPUT="tests.txt"
 gcc c/lu_determinant.c -o bin/lu_determinant -O3
 clang c/lu_determinant.c -o bin/lu_determinant.wasm -O3
 
-for arg in 100 1000 2000 5000 10000; do
+for arg in 100 1000 2000 5000 10000 20000; do
     echo $arg:
     echo "  C:"
     time bin/lu_determinant $arg
