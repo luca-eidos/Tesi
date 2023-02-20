06799fac4e43e975c70175d12a4a4fd9201a9f7b
luca-eidos <luca.giovannini@eidos-tech.com>
2023-02-20 16:06:33 +0100

    update hello world

--- Esempi/01-HelloWorld/compile_wasm.sh
+++ Esempi/01-HelloWorld/compile_wasm.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
 clang demo.c -o demo.wasm
+
+$ echo hello world > test.txt
+$ wasmtime demo.wasm test.txt /tmp/somewhere.txt
+error opening input test.txt: Capabilities insufficient
+
+$ wasmtime --dir=. --dir=/tmp demo.wasm test.txt /tmp/somewhere.txt
+$ cat /tmp/somewhere.txt
+hello world
