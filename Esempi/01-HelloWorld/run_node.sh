c4d1578d58efd1efff7fb93e57a86b7972638357
Luca <luca-eidos@gmail.com>
2023-02-20 16:31:15 +0100

    run node snippet

--- Esempi/01-HelloWorld/run_node.sh
+++ Esempi/01-HelloWorld/run_node.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
-node --experimental-wasi-unstable-preview1 index.mjs
-cat test.copy
+$ node --experimental-wasi-unstable-preview1 index.mjs
+$ cat test.copy
+(node:4908) ExperimentalWarning: WASI is an experimental feature and might change at any time
+(Use `node --trace-warnings ...` to show where the warning was created)
+hello world
