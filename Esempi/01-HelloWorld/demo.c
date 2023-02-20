015f4ca075147d276955f372947e6a16952c5c05
Luca <luca-eidos@gmail.com>
2023-02-20 15:34:25 +0100

    update example£

--- Esempi/01-HelloWorld/demo.c
+++ Esempi/01-HelloWorld/demo.c
@@ -5,32 +5,39 @@
 #include <fcntl.h>
 #include <errno.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     ssize_t n, m;
     char buf[BUFSIZ];
 
-    if (argc != 3) {
+    if (argc != 3)
+    {
         fprintf(stderr, "usage: %s <from> <to>\n", argv[0]);
         exit(1);
     }
 
     int in = open(argv[1], O_RDONLY);
-    if (in < 0) {
+    if (in < 0)
+    {
         fprintf(stderr, "error opening input %s: %s\n", argv[1], strerror(errno));
         exit(1);
     }
 
     int out = open(argv[2], O_WRONLY | O_CREAT, 0660);
-    if (out < 0) {
+    if (out < 0)
+    {
         fprintf(stderr, "error opening output %s: %s\n", argv[2], strerror(errno));
         exit(1);
     }
 
-    while ((n = read(in, buf, BUFSIZ)) > 0) {
+    while ((n = read(in, buf, BUFSIZ)) > 0)
+    {
         char *ptr = buf;
-        while (n > 0) {
+        while (n > 0)
+        {
             m = write(out, ptr, (size_t)n);
-            if (m < 0) {
+            if (m < 0)
+            {
                 fprintf(stderr, "write error: %s\n", strerror(errno));
                 exit(1);
             }
@@ -39,7 +46,8 @@ int main(int argc, char **argv) {
         }
     }
 
-    if (n < 0) {
+    if (n < 0)
+    {
         fprintf(stderr, "read error: %s\n", strerror(errno));
         exit(1);
     }
