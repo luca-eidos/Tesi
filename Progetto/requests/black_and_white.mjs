0b0901ab3e68e6694e9d61ec61d055f987b0a37f
luca-eidos <luca.giovannini@eidos-tech.com>
2023-02-27 15:25:39 +0100

    reformat

--- Progetto/requests/black_and_white.mjs
+++ Progetto/requests/black_and_white.mjs
@@ -1,10 +1,5 @@
-import { WASI } from 'wasi';
-import { argv, env } from 'node:process';
-import { readFile } from 'node:fs/promises';
-import tmp from "tmp";
-import fs from "fs";
-import { runWasi, handleRun } from "./utils.mjs"
+import { handleRun } from "./utils.mjs";
 
 export const toBlackAndWhite = async (req, res) => {
   await handleRun(req, res, "black_and_white");
-}
+};
--- Progetto/requests/utils.mjs
+++ Progetto/requests/utils.mjs
@@ -1,10 +1,10 @@
-import { WASI } from 'wasi';
-import { argv, env } from 'node:process';
-import { readFile } from 'node:fs/promises';
+import { WASI } from "wasi";
+import { argv, env } from "node:process";
+import { readFile } from "node:fs/promises";
 import tmp from "tmp";
 import fs from "fs";
 
-export const tmpFolder = '/tmp/';
+export const tmpFolder = "/tmp/";
 
 /**
  * Middleware for validating input image (if any).
@@ -17,12 +17,17 @@ export const validateImage = (req, res, next) => {
 
   // Check if there's a file in the request body
   if (!imageFile) {
-    return res.status(400).json({ error: 'No file uploaded.' });
+    return res.status(400).json({ error: "No file uploaded." });
   }
 
   // Check if the uploaded file is a JPEG or PNG image
-  if (imageFile.mimetype !== 'image/jpeg' && imageFile.mimetype !== 'image/png') {
-    return res.status(400).json({ error: 'Invalid file type. Please upload a JPEG or PNG image.' });
+  if (
+    imageFile.mimetype !== "image/jpeg" &&
+    imageFile.mimetype !== "image/png"
+  ) {
+    return res
+      .status(400)
+      .json({ error: "Invalid file type. Please upload a JPEG or PNG image." });
   }
 
   next();
@@ -35,29 +40,26 @@ export const validateImage = (req, res, next) => {
  */
 export const runWasi = async (filename, ...wasmArgs) => {
   const wasi = new WASI({
-    args: [
-      argv[0], 
-      ...wasmArgs
-    ],
+    args: [argv[0], ...wasmArgs],
     env,
     preopens: {
-      '/tmp': "/tmp"
-    }
+      "/tmp": "/tmp",
+    },
   });
-  
+
   const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
-  
+
   const wasm = await WebAssembly.compile(
     await readFile(new URL(`../bin/${filename}.wasm`, import.meta.url))
   );
   const instance = await WebAssembly.instantiate(wasm, importObject);
 
   wasi.start(instance);
-}
+};
 
 /**
- * Wrapper function for runWasi function, which handles the tempfile and arguments from request. 
- * @param {Request} req: request object from express 
+ * Wrapper function for runWasi function, which handles the tempfile and arguments from request.
+ * @param {Request} req: request object from express
  * @param {Response} res: response object from express
  * @param {String} filename: WASM file name without extension
  * @param {...String} otherArgs: additional arguments for WASI instance, other than input and output paths
@@ -65,16 +67,16 @@ export const runWasi = async (filename, ...wasmArgs) => {
 export const handleRun = async (req, res, filename, ...otherArgs) => {
   const imagePath = req.file.path;
 
-  const output = tmp.fileSync({ 
-    mode: 0o644, 
-    postfix: (req.file.mimetype === 'image/jpeg')? ".jpg" : ".png" 
+  const output = tmp.fileSync({
+    mode: 0o644,
+    postfix: req.file.mimetype === "image/jpeg" ? ".jpg" : ".png",
   }).name;
   await runWasi(filename, imagePath, output, ...otherArgs);
 
   // Read the uploaded file from disk and send it back to the client
   fs.readFile(output, (err, data) => {
     if (err) {
-      return res.status(500).json({ error: 'Error reading file.' });
+      return res.status(500).json({ error: "Error reading file." });
     }
     res.contentType(req.file.mimetype);
     res.send(data);
@@ -84,4 +86,4 @@ export const handleRun = async (req, res, filename, ...otherArgs) => {
       }
     });
   });
-}
+};
