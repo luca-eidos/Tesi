04faaf53f27eb6a4d175658172fa8e21ed2712e2
Luca <luca-eidos@gmail.com>
2023-02-28 16:52:17 +0100

    example

--- Progetto/c/compileandrun.sh
+++ Progetto/c/compileandrun.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-gcc black_and_white.c Image.c -o black_and_white -std=c17 -Wall -lm
-clang black_and_white.c Image.c -o black_and_white.wasm -std=c17 -Wall -lm -O3
-# ./black_and_white sky.jpg sky_black_white.jpg
-wasmtime ./black_and_white.wasm --dir . sky.jpg sky_black_white.jpg
+# gcc gray_scale.c Image.c -o gray_scale -std=c17 -Wall -lm
+clang gray_scale.c Image.c -o gray_scale.wasm -std=c17 -Wall -lm -O3
+# ./gray_scale sky.jpg sky_black_white.jpg
+wasmtime ./gray_scale.wasm --dir . sky.jpg sky_gray.jpg
+xdg-open sky_gray.jpg
